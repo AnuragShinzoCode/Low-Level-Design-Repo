cd43c17e846cc479706a9d9ecbac019777b03a00
Anurag Singh <anurag.singhya@gmail.com>
2026-09-13 17:10:27 +0530

    Builder Design Pattern

---
+++ src/main/java/org/example/BuilderPattern/Client.java
@@ -0,0 +1,18 @@
+package org.example.BuilderPattern;
+
+public class Client {
+    public static void main(String[] args) {
+    SoldItem ob=SoldItem.getBuilder()       //get Builder metthod will retrun builder object
+            .setName("hello")               //these set methods retrun Builder object only
+            .setQuantity(2)
+            .setId(1).
+            setDis(0.2f).
+            setPrime(true).
+            build();                    //this method is retruing the Solditem object
+        System.out.println("Object   "+ob.getName());
+    }
+
+
+    //usig this pattern the chances of getting error is less if paramaterrs are in large no also
+
+}
---
+++ src/main/java/org/example/BuilderPattern/SoldItem.java
@@ -0,0 +1,72 @@
+package org.example.BuilderPattern;
+
+public class SoldItem {
+    int id;
+    String name;
+    int price;
+    int quantity;
+    Float dis;
+    boolean isPrime;
+
+    static SoldItemBuilder getBuilder(){
+        return new SoldItemBuilder();
+    }
+
+    public SoldItem(SoldItemBuilder soldItemBuilder){
+        this.id=soldItemBuilder.getId();
+        this.name=soldItemBuilder.getName();
+        this.dis=soldItemBuilder.getDis();
+        this.quantity=soldItemBuilder.getQuantity();
+        this.isPrime=soldItemBuilder.isPrime();
+        this.price=soldItemBuilder.getPrice();
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setDis(Float dis) {
+        this.dis = dis;
+    }
+
+    public void setPrime(boolean prime) {
+        isPrime = prime;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Float getDis() {
+        return dis;
+    }
+
+    public boolean isPrime() {
+        return isPrime;
+    }
+}
+
